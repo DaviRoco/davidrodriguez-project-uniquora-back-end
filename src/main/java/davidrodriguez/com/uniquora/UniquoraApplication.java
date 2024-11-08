bd32302a74478ec388d4210b683a4afb337339d8
David Rodríguez Coto <64442948+DaviRoco@users.noreply.github.com>
2024-11-08 08:53:26 -0700

    Initial commit

---
+++ src/main/java/davidrodriguez/com/uniquora/UniquoraApplication.java
@@ -0,0 +1,13 @@
+package davidrodriguez.com.uniquora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniquoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniquoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davidrodriguez/com/uniquora/UniquoraApplicationTests.java
@@ -0,0 +1,13 @@
+package davidrodriguez.com.uniquora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniquoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
