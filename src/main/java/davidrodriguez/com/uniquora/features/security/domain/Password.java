95187a0dfdc4920fdcccffc0b28d9a4708c3c94c
DaviRoco <david@rodriguezcoto.com>
2025-01-30 22:17:35 -0600

    Update to Password Class
    Removed Salt since I will be using BCrypt.

---
+++ src/main/java/davidrodriguez/com/uniquora/features/security/domain/Password.java
@@ -0,0 +1,45 @@
+package davidrodriguez.com.uniquora.features.security.domain;
+
+import jakarta.persistence.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Entity
+@Table(name = "password", schema = "public")
+public class Password {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public Password() {}
+
+    public Password(String plainPassword) {
+        this.password = hashPassword(plainPassword);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String plainPassword) {
+        this.password = hashPassword(plainPassword);
+    }
+
+    private String hashPassword(String plainPassword) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(plainPassword);
+    }
+
+    public boolean matches(String plainPassword) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.matches(plainPassword, this.password);
+    }
+}
