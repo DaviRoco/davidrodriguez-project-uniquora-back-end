693a86bd1e6439f22ee87b7ce9cd1b3e5dff8163
DaviRoco <david@rodriguezcoto.com>
2025-04-09 14:40:29 -0600

    Update LambdaHandler.java

--- src/main/java/davidrodriguez/com/uniquora/configurations/LambdaHandler.java
+++ src/main/java/davidrodriguez/com/uniquora/configurations/LambdaHandler.java
@@ -5,7 +5,6 @@ import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import davidrodriguez.com.uniquora.UniquoraApplication;
 
